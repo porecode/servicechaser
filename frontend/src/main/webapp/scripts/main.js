0187ae3e8d59a9c8e812a9c5030cb318badfdf66
filipovskii_off <filipovskii.off@gmail.com>
2011-10-16 20:29:23 +0400

    bootstrap, main page

--- frontend/src/main/webapp/scripts/main.js
+++ frontend/src/main/webapp/scripts/main.js
@@ -1,5 +1,6 @@
 
-require(["scripts/lib/underscore/underscore.js",
+require(["scripts/lib/less/less-min.js",
+    "scripts/lib/underscore/underscore.js",
     "scripts/lib/backbone/backbone.js"], function() {
   console.log("basic js loaded");
 })
